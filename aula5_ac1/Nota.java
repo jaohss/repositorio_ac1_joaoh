52b753be0a6e596a181db10a2e1e0f8336cf2311
jaohss <joaohsantos01@gmail.com>
2024-03-30 19:32:22 -0300

    alteração

--- aula5_ac1/Nota.java
+++ aula5_ac1/Nota.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Nota {
     public static void main(String[] args) {
+        //Declaração de variáveis
         double ac1=0;
         double ac2=0;
         double af=0;
@@ -13,6 +14,7 @@ public class Nota {
         double totalPond=0;
         Scanner scanner = new Scanner(System.in);
 
+        //Entrada de dados(notas) 
         System.out.println("Digite a nota da AC1:  ");
         ac1 = scanner.nextDouble();  
         
@@ -25,10 +27,12 @@ public class Nota {
         System.out.println("Digite a nota da AF:  ");
         af = scanner.nextDouble(); 
 
-        somaPond=ac1*0.15+ac2*0.3+ag*0.1+af*0.45;
-        totalPond=0.15+0.3+0.45+0.1;
-        media=somaPond/totalPond;
+        //processamento de dados
+        somaPond=ac1*0.15+ac2*0.3+ag*0.1+af*0.45;//soma das notas multiplicada pelo peso da respectiva nota
+        totalPond=0.15+0.3+0.45+0.1;//total do peso da cada nota
+        media=somaPond/totalPond;//Nota final do aluno
 
+        //saida de dados
         System.out.println("A noto final do aluno Fulano de tal foi de: "+media);
 
         scanner.close();
